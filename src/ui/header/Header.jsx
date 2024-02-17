69142177723c2733829ad41697f33cf4ecb17e42
Vladyslav Zyma <vladzima13@gmail.com>
2024-02-17 10:29:30 +0200

    add nav links

--- src/ui/header/Header.jsx
+++ src/ui/header/Header.jsx
@@ -1,7 +1,13 @@
+import Nav from '../nav/Nav';
+
 import styles from './Header.module.css';
 
 function Header() {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <Nav />
+    </header>
+  );
 }
 
 export default Header;
--- src/ui/nav/Nav.jsx
+++ src/ui/nav/Nav.jsx
@@ -1,7 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
 import styles from './Nav.module.css';
 
 function Nav() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to='/home'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/login'>Login</NavLink>
+        </li>
+        <li>
+          <NavLink to='/register'>Register</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
