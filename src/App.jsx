a0d4ccb440c5e2478513f47ef7e31bb670d40789
Vladyslav Zyma <vladzima13@gmail.com>
2024-03-07 13:16:26 +0200

    add Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 // LAYOUTS
 import MainLayout from './layouts/MainLayout';
@@ -45,6 +46,7 @@ function App() {
       </QueryClientProvider>
 
       <Footer />
+      <Toaster />
     </div>
   );
 }
