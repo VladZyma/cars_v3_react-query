265eb96eed6be162b5a624208cde7c458bb6f146
Vladyslav Zyma <vladzima13@gmail.com>
2024-03-07 11:55:35 +0200

    add axios interseptors on request

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
 
 import { baseURL } from '../configs/urls';
+import { oauthService } from './oauth.service';
 
 const axiosService = axios.create({ baseURL });
+
+axiosService.interceptors.request.use((config) => {
+  const access = oauthService.getAccessTokenKey();
+
+  if (access) {
+    config.headers.Authorization = `Bearer ${access}`;
+  }
+
+  return config;
+});
 
 export { axiosService };
