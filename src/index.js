dae7b9bd4c47a97c840ca9e635d60c58d29cef14
Vladyslav Zyma <vladzima13@gmail.com>
2024-03-07 12:09:58 +0200

    add unstable_BrowserHistory

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { unstable_HistoryRouter as BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
+import { history } from './services/axios.service';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter history={history}>
     <App />
   </BrowserRouter>
 );
