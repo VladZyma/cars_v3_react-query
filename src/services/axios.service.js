8cab47c8016d0ae07e2fea28ce9461a5bb50eb84
Vladyslav Zyma <vladzima13@gmail.com>
2024-03-07 12:09:24 +0200

    add axios interceptors on response

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import { createBrowserHistory } from 'history';
 
 import { baseURL } from '../configs/urls';
 import { oauthService } from './oauth.service';
+
+let isRefreshing = false;
+const history = createBrowserHistory();
 
 const axiosService = axios.create({ baseURL });
 
@@ -15,4 +19,30 @@ axiosService.interceptors.request.use((config) => {
   return config;
 });
 
-export { axiosService };
+axiosService.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const refresh = oauthService.getRefreshTokenKey();
+
+    if (error.response?.status === 401 && refresh && !isRefreshing) {
+      isRefreshing = true;
+
+      try {
+        const { data } = oauthService.refresh(refresh);
+        oauthService.setAccessTokenKeys(data);
+      } catch (error) {
+        oauthService.deleteAccessTokenKeys();
+        history.replace('/login?expSession=true');
+      }
+
+      isRefreshing = false;
+      return axiosService(error.config);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export { axiosService, history };
